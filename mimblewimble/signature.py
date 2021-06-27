3867e1e1995308849775db04bd841d0db939cc13
marekyggdrasil <marek.yggdrasil@gmail.com>
2021-06-27 10:47:03 +0800

    signature class implemented

--- mimblewimble/signature.py
+++ mimblewimble/signature.py
@@ -1,4 +1,29 @@
 # TODO
 class Signature:
-    def __init__(self):
-        pass
+    def __init__(self, signatureBytes):
+        self.signatureBytes = signatureBytes
+
+    # operators
+
+    def __eq__(self, other):
+        return self.getSignatureBytes() == other.getSignatureBytes()
+
+    # getters
+
+    def getSignatureBytes(self):
+        return self.signatureBytes
+
+    # serialization / deserialization
+
+    def serialize(self, serializer):
+        serializer.write(self.getSignatureBytes())
+
+    @classmethod
+    def deserialize(self, byteBuffer):
+        return Signature(byteBuffer.read(8))
+
+    def hex(self):
+        return self.getSignatureBytes().hex()
+
+    def format(self):
+        return 'RawSig\{' + self.hex() + '\}'
