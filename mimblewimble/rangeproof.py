f879f266289b6c5189b17a50e51caaa779241e01
marekyggdrasil <marek.yggdrasil@gmail.com>
2021-07-01 21:42:08 +0800

    Implemented the RangeProof class

--- mimblewimble/rangeproof.py
+++ mimblewimble/rangeproof.py
@@ -1,3 +1,40 @@
+import BytesIO
+
+
 class RangeProof:
-    def __init__(self):
-        pass
+    MAX_PROOF_SIZE = 675;
+
+    def __init__(self, proofBytes: bytes):
+        self.proofBytes = proofBytes
+
+    # operators
+
+    def __eq__(self, other):
+        return self.getProofBytes() == other.getProofBytes()
+
+    # getters
+
+    def getProofBytes(self):
+        return self.proofBytes
+
+    # serialization / deserialization
+
+    def serialize(self, serializer):
+        serializer.write(self.getProofBytes())
+
+    @classmethod
+    def deserialize(self, byteBuffer: BytesIO):
+        proofSize = byteBuffer.readall()
+        if len(proofSize) > MAX_PROOF_SIZE:
+            raise ValueError('Proof of size {0} exceeds the maximum'.format(str(len()proofSize)))
+        return RangeProof(proofSize)
+
+    def hex(self):
+        return self.getProofBytes().hex()
+
+    @classmethod
+    def fromhex(self, _hex: str):
+        return RangeProof(bytes.fromhex(_hex))
+
+    def format(self):
+        return self.hex()
