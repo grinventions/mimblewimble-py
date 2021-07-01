4b15d27991cc2044af93aef77e27d74fc675680d
marekyggdrasil <marek.yggdrasil@gmail.com>
2021-07-01 21:30:55 +0800

    Implemented the Commitment class

--- mimblewimble/commitment.py
+++ mimblewimble/commitment.py
@@ -1,5 +1,46 @@
+import BytesIO
 
-# TODO
+
 class Commitment:
-    def __init__(self):
-        pass
+    def __init__(self, commitmentBytes: bytes):
+        self.commitmentBytes = commitmentBytes
+
+    # operators
+
+    def __lt__(self, other):
+        return self.getBytes() < other.getBytes()
+
+    def __eq__(self, other):
+        return self.getBytes() == other.getBytes()
+
+    def __ne__(self, other):
+        return self.getBytes() != other.getBytes()
+
+    # getters
+
+    def getBytes(self):
+        return self.commitmentBytes
+
+    def __int__(self):
+        return int(self.commitmentBytes)
+
+    # serialization / deserialization
+
+    def serialize(self, serializer):
+        serializer.write(self.getBytes())
+
+    @classmethod
+    def deserialize(self, byteBuffer: BytesIO):
+        return Commitment(byteBuffer.read(33))
+
+    def hex(self):
+        return self.commitmentBytes.hex()
+
+    @classmethod
+    def fromHex(self, _hex: str):
+        return Commitment(bytes.fromxex(_hex))
+
+    def format(self):
+        return 'Commitment{' + self.hex() + '}'
+
+
