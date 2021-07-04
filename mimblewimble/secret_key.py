eb886274edb2900067145f3ca7e84ab5475e056a
marekyggdrasil <marek.yggdrasil@gmail.com>
2021-07-04 11:47:57 +0800

    Implemented SecretKey

--- mimblewimble/secret_key.py
+++ mimblewimble/secret_key.py
@@ -1,5 +1,26 @@
 
-# TODO
+import BytesIO
+
+
 class SecretKey:
-    def __init__(self):
-        pass
+    def __init__(self, seed: bytes):
+        self.seed = seed
+        self.NUM_BYTES = len(seed)
+
+    # operators
+
+    def __eq__(self, other):
+        return self.seed == other.seed
+
+    def __ne__(self, other):
+        return self.seed != other.seed
+
+    # serialization / deserialization
+
+    def serialize(self, serializer):
+        serializer.write(self.seed)
+
+    @classmethod
+    def deserialize(self, byteBuffer: BytesIO, NUM_BYTES=32):
+        return SecretKey(byteBuffer.read(NUM_BYTES))
+
