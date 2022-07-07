48e82f53eb1e2a522aec5b08411a3ba060f50f33
marekyggdrasil <marek.yggdrasil@gmail.com>
2022-07-07 11:50:01 +0200

    Implemented mnemonic from entropy method

--- mimblewimble/mnemonic.py
+++ mimblewimble/mnemonic.py
@@ -1,4 +1,7 @@
+import hashlib
 import json
+
+from math import ceil
 
 try:
     import importlib.resources as pkg_resources
@@ -15,10 +18,47 @@ class Mnemonic:
         f = pkg_resources.open_binary(static, 'wordlist.json')
         self.words = json.load(f)
 
-    def mnemonicFromEntropy(self, entropy: int, entropy_len: int):
+    def mnemonicFromEntropy(self, entropy: bytes):
+        entropy_len = len(entropy)
+
         if entropy_len % 4 != 0:
             raise ValueError('Entropy was of incorrect length.')
-        pass
+
+        entropyBits = int(entropy_len * 8)
+        checksumBits = int(entropy_len / 32)
+        numWords = ceil((entropyBits + checksumBits) / 11)
+
+        wordIndices = [0x0 for j in range(numWords)]
+
+        loc = 0
+        for i in range(entropy_len):
+            byte = entropy[i]
+            for j in reversed(range(1, 9)):
+                bit = 1
+                if (byte & (1 << (j - 1))) == 0:
+                    bit = 0
+                wordIndices[int(loc / 11)] |= bit << (10 - (loc % 11))
+                loc += 1
+
+        mask = ((1 << checksumBits) - 1)
+        lhs = int.from_bytes(hashlib.sha256(entropy).digest(), 'big')
+        checksum = (lhs >> (8 - checksumBits)) & mask
+
+        for i in reversed(range(1, checksumBits)):
+            bit = 1
+            if (checksum & (1 << (i - 1))) == 0:
+                bit = 0
+            wordIndices[int(loc / 11)] |= bit << (10 - (loc % 11))
+            loc += 1
+
+        phrase = ''
+        for i in range(numWords):
+            if i > 0:
+                phrase += ' '
+            phrase += self.words[wordIndices[i]]
+
+        return phrase
+
 
 
     def entropyFromMnemonic(self, wallet_words: str):
--- tests/test_mnemonic.py
+++ tests/test_mnemonic.py
@@ -6,8 +6,14 @@ from mimblewimble.mnemonic import Mnemonic
 class MnemonicTests(unittest.TestCase):
     def test_incorrect_length_entropy(self):
         M = Mnemonic()
-        M.mnemonicFromEntropy(4327429364, 12)
+        M.mnemonicFromEntropy(bytes([0x0 for i in range(32)]))
         with self.assertRaises(ValueError) as context:
-            M.mnemonicFromEntropy(4327429364, 13)
+            M.mnemonicFromEntropy(bytes([0x0 for i in range(13)]))
         self.assertTrue(
             'Entropy was of incorrect length.' == str(context.exception))
+
+    def test_mnemonic_from_entropy(self):
+        M = Mnemonic()
+        phrase = M.mnemonicFromEntropy(bytes([0x0 for i in range(32)]))
+        assert phrase == 'abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon'
+
