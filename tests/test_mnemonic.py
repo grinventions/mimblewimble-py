10b6954cc88188062e19c3a70e606e9902d16715
marekyggdrasil <marek.yggdrasil@gmail.com>
2022-07-07 11:13:51 +0200

    testing the entropy length

--- tests/test_mnemonic.py
+++ tests/test_mnemonic.py
@@ -4,5 +4,10 @@ from mimblewimble.mnemonic import Mnemonic
 
 
 class MnemonicTests(unittest.TestCase):
-    def test_incorrect_entropy(self):
+    def test_incorrect_length_entropy(self):
         M = Mnemonic()
+        M.mnemonicFromEntropy(4327429364, 12)
+        with self.assertRaises(ValueError) as context:
+            M.mnemonicFromEntropy(4327429364, 13)
+        self.assertTrue(
+            'Entropy was of incorrect length.' == str(context.exception))
