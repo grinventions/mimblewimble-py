7ee4b2eed1db885bf5524613097b7941ec2d51ca
marekyggdrasil <marek.yggdrasil@gmail.com>
2022-07-23 10:21:21 +0200

    collected test data from the core wallet v5.0.3

---
+++ tests/test_wallet.py
@@ -0,0 +1,18 @@
+# this wallet contains no funds, was generated using grin-wallet 5.0.3 for test purposes,
+# do NOT send any funds to this wallet and if you do, consider them lost as the seed is publicly disclosed
+
+seed = {
+    'encrypted_seed': '839773da8062af7dc51714fd98a7f9a72750e17aa54541d5317b5ea1be5c5751db85497aa630380dd984e2ecd603ae0b',
+    'salt': '356f045acf2b2787',
+    'nonce': 'b5a490e1c942e6a5147bb740'
+}
+
+password = 'grinventions'
+
+recovery_phrase = 'sign interest obtain raw window monster jump bring nice crunch toward grunt prosper recycle sphere battle mother fold reject velvet emotion similar romance govern'
+
+accounts = [
+    ('default', 'm/0/0', 'grin14kgku7l5x6te3arast3p59zk4rteznq2ug6kmmypf2d6z8md76eqg3su35'),
+    ('alice', 'm/1/0 ', 'grin1uqan8sf49yf0369ezef9jhl25jll9fc8xc5wjkcg0w6nv6v85v2sp4wgwy'),
+    ('bob', 'm/2/0', 'grin1guszgjsjlt9vrppu42l03xx080epzzvse5nev3nvdh632explc0sj8ylja'),
+]
