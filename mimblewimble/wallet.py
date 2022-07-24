9fd5bebac57621183b91d07c1688b79a6072259b
marekyggdrasil <marek.yggdrasil@gmail.com>
2022-07-24 16:38:17 +0200

    a lot of WIP stuff

--- mimblewimble/wallet.py
+++ mimblewimble/wallet.py
@@ -1,25 +1,92 @@
 import os
 
-from Crypto.Cipher import AES
+from nacl import bindings
+
+from Crypto.Cipher import ChaCha20_Poly1305
+from Crypto.Random import get_random_bytes
+
+from hashlib import blake2b, pbkdf2_hmac
+
+from bip32 import BIP32
+from bip_utils import Bech32Encoder
+
+from mimblewimble.mnemonic import Mnemonic
 
 
 class Wallet:
-    def __init__(self, encrypted_seed):
+    def __init__(self, encrypted_seed=None, salt=None, nonce=None, master_seed=None):
         self.encrypted_seed = encrypted_seed
-        self.wallet_seed = None
+        self.salt = salt
+        self.nonce = nonce
+        self.master_seed = master_seed
+
+
+    def encryptWallet(self, passphrase, salt=None, nonce=None):
+        if self.master_seed is None:
+            raise Exception('The wallet is shielded')
+        if salt is not None:
+            self.salt = salt
+        if nonce is not None:
+            self.nonce = nonce
+        # TODO compute encrypted_seed from master_seed
+        key = pbkdf2_hmac('sha512', bytes(passphrase, 'utf-8'), salt, 100)
+        cipher = ChaCha20_Poly1305.new(key=key[0:32], nonce=nonce)
+        self.encrypted_seed = cipher.encrypt(self.master_seed)
+
+
+    def shieldWallet(self, salt=None, nonce=None, passphrase=None):
+        if None in [self.encrypted_seed, self.salt, self.nonce]:
+            raise Exception('The wallet cannot be shielded as password is not set')
+        self.master_seed = None
+
+
+    def unshieldWallet(self, passphrase, salt=None, nonce=None):
+        if self.salt is None and salt is None:
+            raise Exception('Missing salt')
+        elif self.salt is None and salt is not None:
+            self.salt = salt
+        if self.nonce is None and nonce is None:
+            raise Exception('Missing nonce')
+        elif self.nonce is None and nonce is not None:
+            self.nonce = nonce
+        # TODO compute master_seed from encrypted_seed
+        key = pbkdf2_hmac('sha512', bytes(passphrase, 'utf-8'), salt, 100)
+        cipher = ChaCha20_Poly1305.new(key=key[0:32], nonce=nonce)
+        self.master_seed = cipher.decrypt(self.encrypted_seed)
+
+
+    def getSeedPhrase(self):
+        if self.master_seed is None:
+            raise Exception('The wallet is shielded')
+        M = Mnemonic()
+        return M.mnemonicFromEntropy(self.master_seed)
+
+
+    def getSlatepackAddress(self, path=None, testnet=False):
+        if self.master_seed is None:
+            raise Exception('The wallet is shielded')
+        bip32 = BIP32.from_seed(self.master_seed)
+        sk_der = bip32.get_privkey_from_path(path)
+        sk_der_blake = blake2b(sk_der, digest_size=64).digest()
+        pk = bindings.crypto_sign_ed25519_sk_to_pk(sk_der_blake)
+        return Bech32Encoder.Encode('grin', pk)
 
 
     @classmethod
-    def initialize(self, num_words=24, passphrase=''):
-        # entropy bytes
-        entropy_bytes = (4 * num_words) / 3
-        # wallet seed
-        wallet_seed = os.urandom(4)
-        # encrypted seed
-        IV = bytes.fromhex(passphrase)
-        encrypted_seed = AES.new(wallet_seed, AES.MODE_CBC, IV=IV)
-        # wallet seed phrase
+    def initialize(self):
         # TODO
-        # initialize the wallet
-        return Wallet(encrypted_seed)
+        pass
+
+
+    @classmethod
+    def fromSeedPhrase(self, phrase: str):
+        M = Mnemonic()
+        master_seed = M.entropyFromMnemonic(phrase)
+        return Wallet(master_seed=master_seed)
+
+
+    @classmethod
+    def fromEncryptedSeed(encrypted_seed_hex: str, salt: str, nonce: str, passphrase: str):
+        # TODO
+        pass
 
--- tests/test_wallet.py
+++ tests/test_wallet.py
@@ -1,3 +1,16 @@
+import pytest
+
+from hashlib import blake2b
+
+from bip32 import BIP32
+from bip_utils import Bech32Encoder
+
+from nacl import bindings
+
+from mimblewimble.wallet import Wallet
+from mimblewimble.mnemonic import Mnemonic
+
+
 # this wallet contains no funds, was generated using grin-wallet 5.0.3 for test purposes,
 # do NOT send any funds to this wallet and if you do, consider them lost as the seed is publicly disclosed
 
@@ -12,7 +25,67 @@ password = 'grinventions'
 recovery_phrase = 'sign interest obtain raw window monster jump bring nice crunch toward grunt prosper recycle sphere battle mother fold reject velvet emotion similar romance govern'
 
 accounts = [
-    ('default', 'm/0/0', 'grin14kgku7l5x6te3arast3p59zk4rteznq2ug6kmmypf2d6z8md76eqg3su35'),
-    ('alice', 'm/1/0 ', 'grin1uqan8sf49yf0369ezef9jhl25jll9fc8xc5wjkcg0w6nv6v85v2sp4wgwy'),
-    ('bob', 'm/2/0', 'grin1guszgjsjlt9vrppu42l03xx080epzzvse5nev3nvdh632explc0sj8ylja'),
+    (
+        'default',
+        'm/0/0',
+        'grin14kgku7l5x6te3arast3p59zk4rteznq2ug6kmmypf2d6z8md76eqg3su35'
+    ),
+    (
+        'alice',
+        'm/1/0',
+        'grin1uqan8sf49yf0369ezef9jhl25jll9fc8xc5wjkcg0w6nv6v85v2sp4wgwy'
+    ),
+    (
+        'bob',
+        'm/2/0',
+        'grin1guszgjsjlt9vrppu42l03xx080epzzvse5nev3nvdh632explc0sj8ylja'
+    ),
 ]
+
+# this simply tests if runs, does not check any correctness
+def test_decrypt():
+    encrypted_seed = bytes.fromhex(seed['encrypted_seed'])
+    nonce = bytes.fromhex(seed['nonce'])
+    salt = bytes.fromhex(seed['salt'])
+    w = Wallet(encrypted_seed=encrypted_seed, nonce=nonce, salt=salt)
+    w.unshieldWallet(password, nonce=nonce, salt=salt)
+    # phrase = w.getSeedPhrase()
+    #print(phrase)
+    w.encryptWallet(password, nonce=nonce, salt=salt)
+    assert w.encrypted_seed == encrypted_seed
+
+
+@pytest.mark.skip(reason='wip')
+def test_from_seed_phrase():
+    w = Wallet.fromSeedPhrase(recovery_phrase)
+    for label, path, expected_address in accounts:
+        address = w.getSlatepackAddress(path=path)
+        print()
+        print(address)
+        print(expected_address)
+
+
+@pytest.mark.skip(reason='wip')
+def test_grin_plusplus():
+    seed_words = 'dehydrate opened lilac elapse subtly prying swept ruby liar veteran wife afloat strained camp tugs pager dual tomorrow aimless boxes saucepan invoke utensils vapidly lilac'
+    print(len(seed_words.split(' ')))
+    M = Mnemonic()
+    master_seed = M.entropyFromMnemonic(seed_words)
+
+    # print('by', bindings.crypto_sign_SECRETKEYBYTES)
+    seed = bytes.fromhex('f9a0e73d3cd533368f75ff63cbd97b2100beffbc339cdfa5c203c1a022d9cf11')
+    assert seed == master_seed
+
+    bip32 = BIP32.from_seed(seed)
+    path = 'm/0/1/0'
+    print()
+    sk_der = bip32.get_privkey_from_path(path)
+    print(sk_der.hex())
+    sk_der_blake = blake2b(sk_der, digest_size=64).digest()
+    pk = bindings.crypto_sign_ed25519_sk_to_pk(sk_der_blake)
+    print()
+    print(pk.hex())
+    print('068131549ec5c3bdcb3d13a855e1b76d179919efd974669292b322f5d59a4ccc')
+    slatepack_address = Bech32Encoder.Encode('grin', pk)
+    print(slatepack_address)
+    print('grin1q6qnz4y7chpmmjeazw59tcdhd5tejx00m96xdy5jkv30t4v6fnxqv9kwer')
