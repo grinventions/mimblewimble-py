8625a99646e16c76289bb1279992e54e7a86b3da
marekyggdrasil <marek.yggdrasil@gmail.com>
2023-01-29 09:22:48 +0100

    Fixed the J generator point in Pedersen commitments class

--- mimblewimble/crypto/pedersen.py
+++ mimblewimble/crypto/pedersen.py
@@ -32,16 +32,13 @@ class Pedersen:
         # see
         # https://github.com/mimblewimble/rust-secp256k1-zkp/blob/304ef7264a0dd9ef4efaaa0e28480cb030c18265/src/constants.rs#L138-L188
         # https://github.com/GrinPlusPlus/GrinPlusPlus/blob/b7b06a1ab6c0e7f7263878f788ea7e4203dcab7c/src/Crypto/SwitchGeneratorPoint.h#L5-L15
-        self.generator_j_pub = secp256k1_ec_pubkey_parse(self.ctx, bytes([
-            0x5f, 0x15, 0x21, 0x36, 0x93, 0x93, 0x01, 0x2a,
-            0x8d, 0x8b, 0x39, 0x7e, 0x9b, 0xf4, 0x54, 0x29,
-            0x2f, 0x5a, 0x1b, 0x3d, 0x38, 0x85, 0x16, 0xc2,
-            0xf3, 0x03, 0xfc, 0x95, 0x67, 0xf5, 0x60, 0xb8,
-            0x3a, 0xc4, 0xc5, 0xa6, 0xdc, 0xa2, 0x01, 0x59,
-            0xfc, 0x56, 0xcf, 0x74, 0x9a, 0xa6, 0xa5, 0x65,
-            0x31, 0x6a, 0xa5, 0x03, 0x74, 0x42, 0x3f, 0x42,
-            0x53, 0x8f, 0xaa, 0x2c, 0xd3, 0x09, 0x3f, 0xa4
-        ]))
+        x = bytes.fromhex(
+            'b860f56795fc03f3c21685383d1b5a2f2954f49b7e398b8d2a0193933621155f')
+        y = bytes.fromhex(
+            'a43f09d32caa8f53423f427403a56a3165a5a69a74cf56fc5901a2dca6c5c43a')
+        generator_j_pub_bytes = bytes([0x04]) + x + y
+        self.generator_j_pub = secp256k1_ec_pubkey_parse(
+            self.ctx, generator_j_pub_bytes)
 
     def __del__(self):
         secp256k1_context_destroy(self.ctx)
