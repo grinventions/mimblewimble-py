f143e3060c7415c9e8a3215b50e7ec28e4324a36
marekyggdrasil <marek.yggdrasil@gmail.com>
2023-03-18 15:53:58 +0100

    Bumping up the version for the release that includes the SRS flow

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='mimblewimble',
-    version='0.1.0',
+    version='0.2.0',
     packages=['mimblewimble'],
     license='MIT',
     description = 'A toolset for processing Grin Mimblewimble data structures',
